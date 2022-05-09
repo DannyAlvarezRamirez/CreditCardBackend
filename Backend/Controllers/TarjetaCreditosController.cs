98511114757ee26ffe52ee902c84f162e1dfc57f
DannyAlvarezRamirez <52975490+DannyAlvarezRamirez@users.noreply.github.com>
2022-05-09 11:39:17 -0600

    Add project files.

---
+++ Backend/Controllers/TarjetaCreditosController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Backend;
+using Backend.Models;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TarjetaCreditosController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TarjetaCreditosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/TarjetaCreditos
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TarjetaCredito>>> GetTarjetaCredito()
+        {
+            return await _context.TarjetaCredito.ToListAsync();
+        }
+
+        // GET: api/TarjetaCreditos/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TarjetaCredito>> GetTarjetaCredito(int id)
+        {
+            var tarjetaCredito = await _context.TarjetaCredito.FindAsync(id);
+
+            if (tarjetaCredito == null)
+            {
+                return NotFound();
+            }
+
+            return tarjetaCredito;
+        }
+
+        // PUT: api/TarjetaCreditos/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTarjetaCredito(int id, TarjetaCredito tarjetaCredito)
+        {
+            if (id != tarjetaCredito.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(tarjetaCredito).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TarjetaCreditoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/TarjetaCreditos
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<TarjetaCredito>> PostTarjetaCredito(TarjetaCredito tarjetaCredito)
+        {
+            _context.TarjetaCredito.Add(tarjetaCredito);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTarjetaCredito", new { id = tarjetaCredito.Id }, tarjetaCredito);
+        }
+
+        // DELETE: api/TarjetaCreditos/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTarjetaCredito(int id)
+        {
+            var tarjetaCredito = await _context.TarjetaCredito.FindAsync(id);
+            if (tarjetaCredito == null)
+            {
+                return NotFound();
+            }
+
+            _context.TarjetaCredito.Remove(tarjetaCredito);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TarjetaCreditoExists(int id)
+        {
+            return _context.TarjetaCredito.Any(e => e.Id == id);
+        }
+    }
+}
